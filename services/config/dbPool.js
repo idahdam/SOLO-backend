5f6adb07dd894f964860b304f83e47bddce6bcd5
Hadi <nasshitell@gmail.com>
2021-08-04 14:48:03 +0700

    postgres db

--- services/config/dbPool.js
+++ services/config/dbPool.js
@@ -2,15 +2,26 @@ const dotenv = require("dotenv");
 const { Pool } = require("pg");
 dotenv.config();
 
+// const databaseConfig = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "testing_aslab",
+//   password: "admin",
+//   port: "5432",
+//   //   ssl: {
+//   //     rejectUnauthorized: false,
+//   //   },
+// });
+
 const databaseConfig = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "testing_aslab",
-  password: "admin",
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
   port: "5432",
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = databaseConfig;
